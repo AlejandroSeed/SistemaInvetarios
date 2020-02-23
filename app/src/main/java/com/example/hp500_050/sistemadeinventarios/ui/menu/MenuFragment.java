0d06fa1dd8201adcf12808caf639e169b5925fc2
Alejandro O <alexcisco123@hotmail.com>
2020-02-22 23:57:11 -0600

    make views

---
+++ app/src/main/java/com/example/hp500_050/sistemadeinventarios/ui/menu/MenuFragment.java
@@ -0,0 +1,36 @@
+package com.example.hp500_050.sistemadeinventarios.ui.menu;
+
+import android.arch.lifecycle.ViewModelProviders;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.hp500_050.sistemadeinventarios.R;
+
+public class MenuFragment extends Fragment {
+
+    private MenuViewModel mViewModel;
+
+    public static MenuFragment newInstance() {
+        return new MenuFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.menu_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(MenuViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/example/hp500_050/sistemadeinventarios/ui/menu/MenuViewModel.java
@@ -0,0 +1,7 @@
+package com.example.hp500_050.sistemadeinventarios.ui.menu;
+
+import android.arch.lifecycle.ViewModel;
+
+public class MenuViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
