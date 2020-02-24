49eb1e3981159419c95c14b61b336ebbe089eb99
Alejandro O <alexcisco123@hotmail.com>
2020-02-23 19:59:13 -0600

    Ending login and navigation view

--- app/src/main/java/com/example/hp500_050/sistemadeinventarios/ui/menu/MenuViewModel.java
+++
@@ -1,7 +0,0 @@
-package com.example.hp500_050.sistemadeinventarios.ui.menu;
-
-import android.arch.lifecycle.ViewModel;
-
-public class MenuViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
-}
